98fd0f472a850d7e658e094e285ac89edc556a58
teng <zhang.ten@husky.neu.edu>
2018-01-26 23:14:04 -0500

    update2

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -40,7 +40,6 @@ class Board extends React.Component {
     }
 
     handleClick(i) {
-        //var letters = ['A','B','D','G','E','F','H','C','A','B','D','E','F','H','G','C'];
         const corrects = this.state.corrects.slice();
 
         if(this.state.letters[i] === this.state.letters[this.state.lastClicked] && i !==this.state.lastClicked) {
@@ -66,13 +65,13 @@ class Board extends React.Component {
     handleRestartClick() {
 
         let shuffledLetters = this.state.letters;
-         shuffledLetters.sort(function(){return Math.random()-0.5;});
-            this.setState({
-                letters: shuffledLetters,
-                lastClicked: null,
-                corrects: Array(16).fill(false),
-                scores: 0,
-            });
+        shuffledLetters.sort(function(){return Math.random()-0.5;});
+        this.setState({
+            letters: shuffledLetters,
+            lastClicked: null,
+            corrects: Array(16).fill(false),
+            scores: 0,
+        });
     }
 
     renderTile(i) {
@@ -83,40 +82,50 @@ class Board extends React.Component {
 
     renderRestartButton() {
         return <RestartButton
-                     onClick={() => this.handleRestartClick()}/>;
+            onClick={() => this.handleRestartClick()}/>;
     }
 
     render() {
-        const status = 'Scores: ' + this.state.scores;
+        const scores = 'Scores: ' + this.state.scores;
 
         return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderTile(0)}
-                    {this.renderTile(1)}
-                    {this.renderTile(2)}
-                    {this.renderTile(3)}
+            <div className="game-board">
+                <div>{scores}</div>
+                <div className="tile-table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>{this.renderTile(0)}</td>
+                            <td>{this.renderTile(1)}</td>
+                            <td>{this.renderTile(2)}</td>
+                            <td>{this.renderTile(3)}</td>
+                        </tr>
+                        <tr>
+
+                            <td>{this.renderTile(4)}</td>
+                            <td>{this.renderTile(5)}</td>
+                            <td>{this.renderTile(6)}</td>
+                            <td>{this.renderTile(7)}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.renderTile(8)}</td>
+                            <td>{this.renderTile(9)}</td>
+                            <td>{this.renderTile(10)}</td>
+                            <td> {this.renderTile(11)}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.renderTile(12)}</td>
+                            <td> {this.renderTile(13)}</td>
+                            <td> {this.renderTile(14)}</td>
+                            <td>{this.renderTile(15)}</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div className="board-row">
-                    {this.renderTile(4)}
-                    {this.renderTile(5)}
-                    {this.renderTile(6)}
-                    {this.renderTile(7)}
-                </div>
-                <div className="board-row">
-                    {this.renderTile(8)}
-                    {this.renderTile(9)}
-                    {this.renderTile(10)}
-                    {this.renderTile(11)}
-                </div>
-                <div className="board-row">
-                    {this.renderTile(12)}
-                    {this.renderTile(13)}
-                    {this.renderTile(14)}
-                    {this.renderTile(15)}
-                </div>
-                {this.renderRestartButton()}
+                <div>{this.renderRestartButton()}</div>
+                <p>Rules: every click on the tile will deduct 1 points from the totall score.<br/>
+                    find a match will get 10 points.</p>
+
             </div>
         );
     }
@@ -126,13 +135,7 @@ class Game extends React.Component {
     render() {
         return (
             <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
+                <Board />
             </div>
         );
     }
